227f103d3b06ec5c85b332f7086e4f4ae1d8b1ca
rahmadnuh-web <rahmadnuh@gmail.com>
2025-10-21 13:45:03 +0700

    Create scrips.js

---
+++ scrips.js
@@ -0,0 +1,7 @@
+// 1. pilih tombol berdasarkan 10-nya
+const tombolSapa = document.getElementById('sapaButton');
+// 2. Tambahkan 'event listener' untuk aksi 'click'
+tombolSapa.addEventlistener('click', function() (
+  // 3. jalankan kode ini ketika tombol di-klik
+                            alert('Halo' Terima kasih sudah berkunjung');
+                            ));
